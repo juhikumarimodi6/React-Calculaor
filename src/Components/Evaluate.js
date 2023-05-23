ed63f0dede8303c55dd3be586f657184d80c2517
juhikumarimodi6 <71226446+juhikumarimodi6@users.noreply.github.com>
2023-05-23 22:56:14 +0530

    Update Evaluate.js

--- src/Components/Evaluate.js
+++ src/Components/Evaluate.js
@@ -1,6 +1,6 @@
 const Evaluate = (input, setResult) => {
     try {
-        setResult(eval(input).toFixed(6).toString())
+        setResult((Math.round((eval(input) + Number.EPSILON) * 10000) / 10000).toString())
     } catch(err) {
         setResult("error")
     }
