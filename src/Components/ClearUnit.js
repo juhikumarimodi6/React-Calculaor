2638d459ad333fcd8f358b28a9b098d2cf5af207
juhikumarimodi6 <71226446+juhikumarimodi6@users.noreply.github.com>
2023-05-24 09:33:56 +0530

    Add files via upload

--- src/Components/ClearUnit.js
+++ src/Components/ClearUnit.js
@@ -7,14 +7,14 @@ const ClearUnit = ({input, setInput,setResult,setDotCountNum}) => {
 
     return (
         <div className="clear">
-            <Clear 
-                setInput = {setInput}
-                setResult={setResult}
-                setDotCountNum = {setDotCountNum }
-            />
             <BackSpaceClear 
                 input = {input} 
                 setInput = {setInput} 
+                setDotCountNum = {setDotCountNum }
+            />
+            <Clear 
+                setInput = {setInput}
+                setResult={setResult}
                 setDotCountNum = {setDotCountNum }
             />
       </div>
--- src/Components/Evaluate.js
+++ src/Components/Evaluate.js
@@ -1,6 +1,7 @@
 const Evaluate = (input, setResult) => {
     try {
         setResult((Math.round((eval(input) + Number.EPSILON) * 10000) / 10000).toString())
+        // setResult((Math.round((Function("return " + input)() + Number.EPSILON) * 10000) / 10000).toString())
     } catch(err) {
         setResult("error")
     }
